7f1f878a5b8b4f198097ff32e54bbbbbc3cceeff
CucNg <vancucit@gmail.com>
2017-05-10 09:44:37 +0700

    update source

---
+++ MyApp/ЯealFriendTests/_ealFriendTests.swift
@@ -0,0 +1,39 @@
+//
+//  _ealFriendTests.swift
+//  ЯealFriendTests
+//
+//  Created by Cuc Nguyen on 5/10/17.
+//  Copyright © 2017 Kuccu. All rights reserved.
+//
+
+import XCTest
+
+class _ealFriendTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let app = UIApplication.shared
+//        let rootView = app.delegate as! UIViewController
+        XCTAssert(true, "Pass")
+
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
