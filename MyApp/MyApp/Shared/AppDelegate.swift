945d5282b7f4cab44fde72c669686d85eba4a62c
CucNg <vancucit@gmail.com>
2017-07-14 16:14:44 +0700

    no message

--- MyApp/MyApp/Shared/AppDelegate.swift
+++ MyApp/MyApp/Shared/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-//        FIRApp.configure()
+        FIRApp.configure()
 
         return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
 //        return true
